fe2ef4d97500b5eddc39bd163513bfe88a68bc29
Ana Ovcharova <akovcharova@gmail.com>
2015-09-17 05:24:29 -0700

    batch submission script can now be used in both UCSB and UCSD

--- scripts/get_flist.py
+++ scripts/get_flist.py
@@ -1,32 +1,50 @@
-import os 
+import os, sys 
 import glob
 import string
 
 # List of datasets to run over, could be either MC or data
-# Enter dataset name exactly as listed in CMS DAS
+# Enter either the dataset path starting with "/store", e.g:
+# /store/mc/RunIISpring15DR74/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/
+# or the dataset name as found in DAS, e.g:
+# /TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM
 datasets = []
 datasets.append('/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM')
-datasets.append('/SingleMuon/Run2015B-PromptReco-v1/MINIAOD')
+datasets.append('/store/data/Run2015B/HTMHT/MINIAOD/PromptReco-v1')
+
+
 
 # Directory to dump all condor-related logs, schell and cmd files
-subdir = "sub/"
-if not (os.path.exists(os.getcwd()+'/'+subdir)):
-    os.mkdir(os.getcwd()+'/'+subdir)
+rundir = "run/"
+if not (os.path.exists(os.getcwd()+'/'+rundir)):
+    os.mkdir(os.getcwd()+'/'+rundir)
+
+# Parsing where the files can be found depends on whether we run on UCSB or UCSD
+host = os.environ.get("HOSTNAME")
+if "ucsd" in host: host = "sd"
+elif host=="compute-0-1.local" or host=="compute-0-0.local": host = "sb"
+elif "ucsb" in host: sys.exit("\033[91mERROR: To allow access to hadoop use cms18 or cms19. \033[0m") 
+else: sys.exit("\033[91mERROR: Unknown host: "+host+" Exit. \033[0m")
+
+hadoop = '/mnt/hadoop/cms'
+if host=="sd": hadoop = '/hadoop/cms/phedex'
 
 for ds in datasets:
     # parse the dataset name and guess the path on hadoop to create the input file list
-    isdata = False # in case we want to pass this as argument to the job later
-    path = ''
-    tags = string.split(ds,'/')
-    dsname = tags[1]
-    campaign = (string.split(tags[2],'-'))[0]
-    reco = tags[2][len(campaign)+1:]
-    filetype = tags[3]
-    if 'PromptReco' in ds:
-        path = '/'.join(['/mnt/hadoop/cms/store/data',campaign,dsname,filetype,reco,'*/*/*/*/*root'])
-        isdata = True
+    path,dsname,campaign,reco = '','','',''
+    if (ds[0:6]=='/store'):
+        tags = string.split(ds,'/')
+        dsname = tags[4]
+        campaign = tags[3]
+        reco = tags[6]
+        filetype = tags[5]
     else:
-        path = '/'.join(['/mnt/hadoop/cms/store/mc',campaign,dsname,filetype,reco,'*/*root'])
+        tags = string.split(ds,'/')
+        dsname = tags[1]
+        campaign = (string.split(tags[2],'-'))[0]
+        reco = tags[2][len(campaign)+1:]
+        filetype = tags[3]
+    if 'PromptReco' in ds: path = '/'.join([hadoop+'/store/data',campaign,dsname,filetype,reco,'*/*/*/*/*root'])
+    else: path = '/'.join([hadoop+'/store/mc',campaign,dsname,filetype,reco,'*/*root'])
 
     filelist = glob.glob(path)
     nfiles = len(filelist)
@@ -35,8 +53,8 @@ for ds in datasets:
         continue
     
     fnm = '_'.join(['flist',dsname,campaign,reco+'.txt'])
-    f = open(subdir+'/'+fnm,"w")
+    f = open(rundir+'/'+fnm,"w")
     for file in filelist:
-        file = string.replace(file,'/mnt/hadoop/cms','')
+        file = string.replace(file,hadoop,'')
         f.write(file+'\n')
     f.close()
--- scripts/sub_cond.py
+++ scripts/sub_cond.py
@@ -1,29 +1,78 @@
-import os, sys 
-import time
+import os, sys, subprocess
 import glob
 import string
+import time
 
-# Maximum number of files per condor job
-maxfiles = 1
+# Maximum number of input MINIAOD files per condor job
+maxfiles = 20
+
+cmssw = "CMSSW_7_4_6_patch6"
+
+# Only matters if running on UCSD:
+# To run on multiple T2's use, e.g:
+# whitelist = "T2_US_UCSD,T2_US_Wisconsin,T2_US_Nebraska,T2_US_Caltech"
+# To run only at UCSD use:
+whitelist = "T2_US_UCSD"
+
+# Condor set up depends on whether we run on UCSB or UCSD
+host = os.environ.get("HOSTNAME")
+if "ucsd" in host: host = "sd"
+elif host=="cms0.physics.ucsb.edu": host = "sb"
+elif ("ucsb" in host) or ("compute" in host): sys.exit("\033[91mERROR: Submission must be done from cms0. \033[0m")
+else: sys.exit("\033[91mERROR: Unknown host: "+host+" Exit. \033[0m")
+print "INFO: Setting up job submission at",('UCSB.' if host=='sb' else 'UCSD.')
 
 # Job submission should be done from the babymaker directory, which is under a valid CMSSW release
 codedir = os.getcwd()
-if not 'CMSSW_7_4_6_patch6/src/babymaker' in codedir:
-    print "\033[91m ERROR: Please submit from path consistent with: <basedir>/CMSSW_7_4_6_patch6/src/babymaker/ \033[0m"
+if not (cmssw+"/src/babymaker") in codedir:
+    print "\033[91m ERROR: Please submit from path consistent with: <basedir>/"+cmssw+"/src/babymaker/ \033[0m"
     sys.exit(0)
 
-# Default output directory is the "out" sub-directory of the current working directory. It is created if it does not exist
-outdir = os.getcwd()+'/out'
-if not (os.path.exists(outdir)):
-    os.mkdir(outdir)
+# Default output directory is the "out" sub-directory of the current working directory.
+outdir = os.getcwd()+'/out/'
+if not (os.path.exists(os.path.realpath(outdir))):
+    sys.exit("\033[91m ERROR: Directory",outdir,"does not exist. Please either create a sym link or dir. \033[0m")
+outdir = os.path.join(os.path.realpath(outdir),time.strftime("%y%m%d_%H%M%S", time.gmtime()))
+os.mkdir(outdir)
+logdir = os.path.join("logs",time.strftime("%y%m%d_%H%M%S", time.gmtime()))
+if not os.path.exists(logdir):
+    if (host=='sd') or (host=='sb' and ('hadoop' not in logdir)): 
+        os.makedirs(logdir)
+    else: 
+        print "\033[91m ERROR: Cannot create log directory on hadoop at UCSB. Using ./logs/ instead. \033[0m" 
+        logdir = './logs/'
+        if not os.path.exists(logdir): os.mkdir(logdir)
+print "INFO: Babies will be written to: ", outdir
+print "INFO: Logs will be written to:   ", logdir
 
 # Get list of datasets and corresponding list of files
-flists = glob.glob("sub/flist*.txt")
+flists = glob.glob("run/flist*.txt")
 filedict = {}
 for fnm in flists:
-    dsname = string.replace(string.replace(fnm,"sub/flist_",''),".txt",'')
+    dsname = string.replace(string.replace(fnm,"run/flist_",""),".txt","")
+    print "INFO: Adding PD: ",dsname
     with open(fnm) as f:
         filedict[dsname] = f.read().splitlines()
+
+# Need a valid proxy to submit condor jobs at UCSD
+proxy,valid = "",""
+if host=="sd":
+    proc = subprocess.Popen('voms-proxy-info', stdout=subprocess.PIPE)
+    tmp = proc.stdout.read()
+    if "Proxy not found" in tmp: 
+        sys.exit("\033[91mERROR: Proxy not found. \033[0m")
+    else: 
+        for info in tmp.splitlines():
+            if ("/tmp/x509" in info): proxy = "/tmp/x509"+(string.split(info,"/tmp/x509"))[1]
+            if ("timeleft" in info): valid = string.replace(info, "timeleft", "Time left before proxy expires")
+
+    print "INFO: Found proxy path",proxy
+    print "INFO:",valid
+
+# If on UCSD prep also tarball
+if (host=="sd"): 
+    print "INFO: Creating babymaker tarball to transfer to work node..."
+    os.system("tar --directory=../ --exclude=\"babymaker/out\" --exclude=\"babymaker/run\" -c babymaker | xz > ../babymaker.tar.xz")
 
 for ds in filedict.keys():
 
@@ -33,45 +82,74 @@ for ds in filedict.keys():
 
     for job in range(0,njobs):
         # name the baby
-        bname = '_'.join(['baby',ds,'batch'+str(job)])
+        bname = "_".join(["baby",ds,"mf"+str(maxfiles),"batch"+str(job)])
 
         # check if job had already succeeded on previous submission
-        outpath = os.path.join(outdir,bname+'.root')
+        outpath = os.path.join(outdir,bname+".root")
         if os.path.exists(outpath):
-            print "\033[93m WARNING: "+outpath+" already exists. Skip job submission. \033[0m"
+            print "\033[38m WARNING: "+outpath+" already exists. Skip job submission. \033[0m"
             continue
 
         # list of arguments to the cmsRun job
         args = []
-        args.append('inputFiles=\\\n'+",\\\n".join(filedict[ds][(job*maxfiles):((job+1)*maxfiles)]))
-        args.append('outputFile='+outpath)
+        args.append("inputFiles=\\\n"+",\\\n".join(filedict[ds][(job*maxfiles):((job+1)*maxfiles)]))
+        if (host=="sb"): args.append("outputFile="+outpath)
+        else: args.append("outputFile="+bname+".root")
 
         # Create executable that will be transfered to the work node by condor
-        exefile ='sub/'+bname+".sh"
+        exefile ="run/"+bname+".sh"
         fexe = open(exefile,"w")
-        fexe.write("#! /bin/bash\n")
-        fexe.write(". /cvmfs/cms.cern.ch/cmsset_default.sh\n")
-        fexe.write("cd "+codedir+"\n")
-        fexe.write("eval `scramv1 runtime -sh`\n")
-        fexe.write("echo $CMSSW_BASE\n")
-        fexe.write("ls -tr\n")
-        fexe.write("cmsRun bmaker/python/bmaker_basic_cfg.py \\\n"+' \\\n'.join(args)+"\n")
+        if (host=="sb"):
+            fexe.write("#! /bin/bash\n")
+            fexe.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n")
+            fexe.write("cd "+codedir+"\n")
+            fexe.write("eval `scramv1 runtime -sh`\n")
+            fexe.write("cmsRun bmaker/python/bmaker_basic_cfg.py \\\n"+" \\\n".join(args)+"\n")
+        else: 
+            fexe.write("#! /bin/bash\n")
+            fexe.write("source /code/osgcode/cmssoft/cmsset_default.sh\n")
+            fexe.write("export SCRAM_ARCH=slc6_amd64_gcc491\n")
+            fexe.write("eval `scramv1 project CMSSW "+cmssw+"`\n")
+            fexe.write("cd "+cmssw+"/src\n")
+            fexe.write("eval `scramv1 runtime -sh`\n")
+            fexe.write("tar -xf ../../babymaker.tar.xz\n")
+            fexe.write("cd babymaker\n")
+            fexe.write("./compile.sh\n")
+            fexe.write("cmsRun bmaker/python/bmaker_basic_cfg.py \\\n"+" \\\n".join(args)+"\n")
+            fexe.write("lcg-cp -b -D srmv2 --vo cms -t 2400 --verbose file:"+bname+".root srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN="+outpath+"\n")
+            fexe.write("cd ../../..\n")
+            fexe.write("rm -rf "+cmssw+"\n")
         fexe.close()
         os.system("chmod u+x "+exefile)
 
         # Create condor submission cmd file
-        cmdfile = 'sub/'+bname+'.cmd'
+        cmdfile = "run/"+bname+".cmd"
         fcmd = open(cmdfile,"w")
-        fcmd.write("Executable   = "+exefile+"\n")
-        fcmd.write("Universe     = vanilla\n")
-        fcmd.write("Log          = sub/"+bname+".log\n")
-        fcmd.write("output       = sub/"+bname+".out\n")
-        fcmd.write("error        = sub/"+bname+".err\n")
-        fcmd.write("Notification = never\n")
-        fcmd.write("Queue\n")
+        if (host=="sb"):
+            fcmd.write("Executable   = "+exefile+"\n")
+            fcmd.write("Universe     = vanilla\n")
+            fcmd.write("Log          = "+logdir+ "/"+bname+".log\n")
+            fcmd.write("output       = "+logdir+"/"+bname+".out\n")
+            fcmd.write("error        = "+logdir+"/"+bname+".err\n")
+            fcmd.write("Notification = never\n")
+            fcmd.write("Queue\n")
+        else:
+            fcmd.write("Universe = grid\n")
+            fcmd.write("Grid_Resource = condor cmssubmit-r1.t2.ucsd.edu glidein-collector.t2.ucsd.edu\n")
+            fcmd.write("x509userproxy="+proxy+"\n")
+            fcmd.write("+remote_DESIRED_Sites=\""+whitelist+"\"\n")
+            fcmd.write("Executable = "+exefile+"\n")
+            fcmd.write("Transfer_Executable = True\n")
+            fcmd.write("should_transfer_files = YES\n")
+            fcmd.write("transfer_input_files = ../babymaker.tar.xz\n")
+            fcmd.write("Notification = Never\n")
+            fcmd.write("Log          = "+logdir+"/"+bname+".log\n")
+            fcmd.write("output       = "+logdir+"/"+bname+".out\n")
+            fcmd.write("error        = "+logdir+"/"+bname+".err\n")
+            fcmd.write("queue 1\n")
         fcmd.close()
 
         # Submit condor job
         cmd = "condor_submit " + cmdfile
-        print cmd
+        print "INFO: Submitting", cmdfile
         os.system(cmd)

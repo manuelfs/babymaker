cda5ecfac427b1f322bfcb1c89ead0603a5d178f
Rohan Bhandari <rohan.bhandari@cern.ch>
2015-11-25 15:30:11 +0100

    change_branch can now take multiples at a time

--- bmaker/genfiles/inc/change_branch.hh
+++ bmaker/genfiles/inc/change_branch.hh
@@ -1,43 +1,57 @@
 #ifndef H_CHANGE_BRANCH
 #define H_CHANGE_BRANCH
 
+#include <deque>
 #include <iostream>
 #include "TFile.h"
 #include "TString.h"
 #include "TTree.h"
+#include <vector>
 #include "utilities.hh"
 
 using namespace std;
 
-void change_branch_one(TString indir, TString name, TString outdir, TString var_type, TString var, TString var_val){
+void change_branch_one(TString indir, TString name, TString outdir, vector<TString> var_type, vector<TString> var, vector<TString> var_val){
 
-  //Set multiply
-  bool multiply=false;
-  if(var_val.BeginsWith("*") || var_val.EndsWith("*")){
-    var_val.Remove(TString::kBoth,'*');
-    multiply=true;
+  if(var_type.size()!=var.size() || var_type.size()!=var_val.size())
+    { cout<<"Error: Branch vectors are not the same size"<<endl; exit(0); }
+
+  const int nvar = var.size();
+
+  //Setup
+  vector<bool> multiply(nvar,false);
+  for(int isetup=0; isetup<nvar; isetup++){
+    //Set multiply
+    if(var_val[isetup].BeginsWith("*") || var_val[isetup].EndsWith("*")){
+      var_val[isetup].Remove(TString::kBoth,'*');
+      multiply[isetup]=true;
+    }
+    
+    //Handle bools
+    if(var_val[isetup]=="true")
+      var_val[isetup]="1";
+    else if(var_val[isetup]=="false")
+      var_val[isetup]="0";
   }
 
-  //Handle bools
-  if(var_val=="true")
-    var_val=="1";
-  else if(var_val=="false")
-    var_val=="0";
-  
   //Set up old tree and branch
   TFile* oldfile = new TFile(indir+name);
   TTree* oldtree = static_cast<TTree*>(oldfile->Get("tree"));
-  
-  int new_var_int_;
-  float new_var_flt_;
-  double new_var_dbl_;
-  bool new_var_bool_;
 
-  if(var_type=="int"){           new_var_int_ = 0;     oldtree->SetBranchAddress(var, &new_var_int_); }
-  else if(var_type=="float"){    new_var_flt_ = 0;   oldtree->SetBranchAddress(var, &new_var_flt_); }
-  else if(var_type=="double"){   new_var_dbl_ = 0;  oldtree->SetBranchAddress(var, &new_var_dbl_); }
-  else if(var_type=="bool"){     new_var_bool_ = 0;    oldtree->SetBranchAddress(var, &new_var_bool_); }
-  else {cout<<"Branch type invalid: Use only \"int\", \"float\", \"double\", or \"bool\""<<endl; exit(0);}
+  vector<int> new_var_int_(nvar,-999);
+  vector<float> new_var_flt_(nvar,-999);
+  vector<double> new_var_dbl_(nvar,-999);
+  deque<bool> new_var_bool_(nvar, false); // vector<bool>is not a vector in C++... so can't pass bools by reference
+  
+  //Branches
+  for(int ibch=0; ibch<nvar; ibch++){
+    if(var_type[ibch]=="int")         {  new_var_int_[ibch]  = 0;     oldtree->SetBranchAddress(var[ibch], &new_var_int_[ibch]); }
+    else if(var_type[ibch]=="float")  {  new_var_flt_[ibch]  = 0;     oldtree->SetBranchAddress(var[ibch], &new_var_flt_[ibch]); }
+    else if(var_type[ibch]=="double") {  new_var_dbl_[ibch]  = 0;     oldtree->SetBranchAddress(var[ibch], &new_var_dbl_[ibch]); }
+    else if(var_type[ibch]=="bool")   {  new_var_bool_[ibch] = 0;     oldtree->SetBranchAddress(var[ibch], &new_var_bool_[ibch]);
+      if(multiply[ibch])  cout<<"Warning: Multiplying branch of type \"bool\". Skipping branch."<<endl;}
+    else {cout<<"Error: Branch type invalid. Use only \"int\", \"float\", \"double\", or \"bool\""<<endl; exit(0);}
+  }
 
   //Set up new tree
   name.ReplaceAll(".root","_mod.root");
@@ -48,18 +62,20 @@ void change_branch_one(TString indir, TString name, TString outdir, TString var_
   int nentries = oldtree->GetEntries();
   for(int i=0; i<nentries; i++){
     oldtree->GetEntry(i);
-
-    if(!multiply){
-      if(var_type=="int")             new_var_int_ = var_val.Atoi();
-      else if(var_type=="float")      new_var_flt_ = var_val.Atof();
-      else if(var_type=="double")     new_var_dbl_ = static_cast<double>(var_val.Atof());
-      else if(var_type=="bool")       new_var_bool_ = var_val.Atoi();
-    }
-    else {
-      if(var_type=="int")           { new_var_int_ *= var_val.Atoi(); }
-      else if(var_type=="float")    { new_var_flt_ *= var_val.Atof(); }
-      else if(var_type=="double")   { new_var_dbl_ *= var_val.Atof(); }
-      else if(var_type=="bool")     { new_var_bool_ *= var_val.Atof(); cout<<"Warning multiplying branch of type \"bool\""<<endl;}
+    
+    //Set vars
+    for(int iset=0; iset<nvar; iset++){
+      if(!multiply[iset]){
+	if(var_type[iset]=="int")             new_var_int_[iset]  = var_val[iset].Atoi();
+	else if(var_type[iset]=="float")      new_var_flt_[iset]  = var_val[iset].Atof();
+	else if(var_type[iset]=="double")     new_var_dbl_[iset]  = static_cast<double>(var_val[iset].Atof());
+	else if(var_type[iset]=="bool")       new_var_bool_[iset] = var_val[iset].Atoi();
+      }
+      else {
+	if(var_type[iset]=="int")             new_var_int_[iset]  *= var_val[iset].Atoi(); 
+	else if(var_type[iset]=="float")      new_var_flt_[iset]  *= var_val[iset].Atof(); 
+	else if(var_type[iset]=="double")     new_var_dbl_[iset]  *= var_val[iset].Atof(); 
+      }
     }
     newtree->Fill();
   }
--- bmaker/genfiles/src/change_branch.cxx
+++ bmaker/genfiles/src/change_branch.cxx
@@ -11,15 +11,23 @@ using namespace std;
 int main(int argc, char *argv[]){
 
   if(argc<5){
-    cout<<"Format: ./run/change_branch.exe <infolder> <outfolder> <branch type> <branch name> <branch value>"<<endl;
-    cout<<"<branch type> must be \"int\", \"float\", \"double\", or \"bool\""<<endl
-    cout<<"Accepts multiplication of branches, i.e. <branch value>=\"*3.1415\" or <branch value>=\"3.1415*\""<<endl;
+    cout<<"Format: ./run/change_branch.exe <infolder> <outfolder> <branch_type1> <branch_name1> <branch_value1> ... <branch_valueN>"<<endl;
+    cout<<"Accepts up to N branches folowing the format above."<<endl;
+    cout<<"Accepts multiplication of branches, i.e. <branch_value>=\"*3.1415\" or <branch_value>=\"3.1415*\""<<endl;
+    cout<<"<branch_type> must be \"int\", \"float\", \"double\", or \"bool\""<<endl;
     return 1;
   }
 
-  TString folder(argv[1]), outfolder(argv[2]), var_type(argv[3]), var(argv[4]), var_val(argv[5]);
+  TString folder(argv[1]), outfolder(argv[2]);
   if(!folder.EndsWith("/")) folder.Append("/");
   if(!outfolder.EndsWith("/")) outfolder.Append("/");
+
+  vector<TString> var_type, var, var_val;
+  for(int ivar=3; ivar<argc; ivar+=3){
+    var_type.push_back(argv[ivar]);
+    var.push_back(argv[ivar+1]);
+    var_val.push_back(argv[ivar+2]);
+  }
 
   vector<TString> files = dirlist(folder,".root");
   

38195e7f59403ca0a11629f3355c605dca7a7399
Adam Dishaw <aldishaw@gmail.com>
2016-03-25 17:29:21 -0700

    Remove w_pdf

--- bmaker/genfiles/src/change_weights.cxx
+++ bmaker/genfiles/src/change_weights.cxx
@@ -17,7 +17,7 @@
 using namespace std;
 
 namespace{
-  const int NSYSTS = 19;
+  const int NSYSTS = 18;
 }
 
 int main(int argc, char *argv[]){
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]){
   vector<TString> var_type, var; 
   vector<vector<TString> > var_val(NSYSTS);
   double nent_eff=0, nent=0, sum_weff_l0=0, sum_weff_l1=0, sum_btag=0, sum_pu=0, sum_toppt=0;
-  vector<double> sum_wpdf(ch.w_pdf().size(),0);
+  //vector<double> sum_wpdf(ch.w_pdf().size(),0);
   vector<double> sum_bctag(ch.sys_bctag().size()), sum_udsgtag(ch.sys_udsgtag().size());
   vector<double> sum_fs_bctag(ch.sys_fs_bctag().size()), sum_fs_udsgtag(ch.sys_fs_udsgtag().size());
   vector<double> sum_isr(ch.sys_isr().size());
@@ -94,7 +94,7 @@ int main(int argc, char *argv[]){
     sum_btag  +=  noNaN(ch.w_btag());
     sum_pu    +=  noNaN(ch.w_pu());
     sum_toppt +=  noNaN(ch.w_toppt());
-    for(unsigned int isys=0;isys<ch.w_pdf().size();isys++)           sum_wpdf[isys]        +=  noNaN(ch.w_pdf().at(isys));
+    //for(unsigned int isys=0;isys<ch.w_pdf().size();isys++)           sum_wpdf[isys]        +=  noNaN(ch.w_pdf().at(isys));
     for(unsigned int isys=0;isys<ch.sys_bctag().size();isys++)       sum_bctag[isys]       +=  noNaN(ch.sys_bctag().at(isys));
     for(unsigned int isys=0;isys<ch.sys_udsgtag().size();isys++)     sum_udsgtag[isys]     +=  noNaN(ch.sys_udsgtag().at(isys));
     for(unsigned int isys=0;isys<ch.sys_fs_bctag().size();isys++)    sum_fs_bctag[isys]    +=  noNaN(ch.sys_fs_bctag().at(isys));
@@ -110,13 +110,13 @@ int main(int argc, char *argv[]){
     }
 
     // Hack to recompute sys_pdf[1] which had a 1e-3 cut
-    float minpdf(1e10);
-    for(unsigned int isys=0;isys<ch.w_pdf().size();isys++)  
-      if(ch.w_pdf()[isys] < minpdf) minpdf = ch.w_pdf()[isys];
-    if(ch.w_pdf().size()==0)
-      sum_pdf_min += 1;
-    else
-      sum_pdf_min += minpdf;
+    //float minpdf(1e10);
+    //for(unsigned int isys=0;isys<ch.w_pdf().size();isys++)  
+    //if(ch.w_pdf()[isys] < minpdf) minpdf = ch.w_pdf()[isys];
+    //if(ch.w_pdf().size()==0)
+    //sum_pdf_min += 1;
+    //else
+    //sum_pdf_min += minpdf;
 				      
     double weight = noNaN(ch.w_lep()) * noNaN(ch.w_fs_lep()) * noNaN(ch.w_toppt()) * noNaN(ch.w_btag());
     //Lepton weights
@@ -162,7 +162,7 @@ int main(int argc, char *argv[]){
   var_type.push_back("float");      var.push_back("w_btag");            
   var_type.push_back("float");      var.push_back("w_pu");              
   var_type.push_back("float");      var.push_back("w_toppt");           
-  var_type.push_back("vfloat");     var.push_back("w_pdf");             
+  //var_type.push_back("vfloat");     var.push_back("w_pdf");             
   var_type.push_back("vfloat");     var.push_back("sys_bctag");         
   var_type.push_back("vfloat");     var.push_back("sys_udsgtag");       
   var_type.push_back("vfloat");     var.push_back("sys_fs_bctag");      
@@ -186,24 +186,24 @@ int main(int argc, char *argv[]){
   var_val[1].push_back("*"+to_string(nent/sum_btag));
   var_val[2].push_back("*"+to_string(nent/sum_pu));
   var_val[3].push_back("*"+to_string(nent*wanted_toppt/sum_toppt));
-  for(unsigned int idx=0;idx<sum_wpdf.size();idx++)         var_val[4].push_back("*"+to_string(nent/sum_wpdf[idx]));
-  for(unsigned int idx=0;idx<sum_bctag.size();idx++)        var_val[5].push_back("*"+to_string(nent/sum_bctag[idx]));
-  for(unsigned int idx=0;idx<sum_udsgtag.size();idx++)      var_val[6].push_back("*"+to_string(nent/sum_udsgtag[idx]));
-  for(unsigned int idx=0;idx<sum_fs_bctag.size();idx++)     var_val[7].push_back("*"+to_string(nent/sum_fs_bctag[idx]));
-  for(unsigned int idx=0;idx<sum_fs_udsgtag.size();idx++)   var_val[8].push_back("*"+to_string(nent/sum_fs_udsgtag[idx]));
-  for(unsigned int idx=0;idx<sum_isr.size();idx++)          var_val[9].push_back("*"+to_string(nent/sum_isr[idx]));
-  for(unsigned int idx=0;idx<sum_spdf.size();idx++)         var_val[10].push_back("*"+to_string(nent/sum_spdf[idx]));
-  for(unsigned int idx=0;idx<sum_mur.size();idx++)          var_val[11].push_back("*"+to_string(nent/sum_mur[idx]));
-  for(unsigned int idx=0;idx<sum_muf.size();idx++)          var_val[12].push_back("*"+to_string(nent/sum_muf[idx]));
-  for(unsigned int idx=0;idx<sum_murf.size();idx++)         var_val[13].push_back("*"+to_string(nent/sum_murf[idx]));
+  //for(unsigned int idx=0;idx<sum_wpdf.size();idx++)         var_val[4].push_back("*"+to_string(nent/sum_wpdf[idx]));
+  for(unsigned int idx=0;idx<sum_bctag.size();idx++)        var_val[4].push_back("*"+to_string(nent/sum_bctag[idx]));
+  for(unsigned int idx=0;idx<sum_udsgtag.size();idx++)      var_val[5].push_back("*"+to_string(nent/sum_udsgtag[idx]));
+  for(unsigned int idx=0;idx<sum_fs_bctag.size();idx++)     var_val[6].push_back("*"+to_string(nent/sum_fs_bctag[idx]));
+  for(unsigned int idx=0;idx<sum_fs_udsgtag.size();idx++)   var_val[7].push_back("*"+to_string(nent/sum_fs_udsgtag[idx]));
+  for(unsigned int idx=0;idx<sum_isr.size();idx++)          var_val[8].push_back("*"+to_string(nent/sum_isr[idx]));
+  for(unsigned int idx=0;idx<sum_spdf.size();idx++)         var_val[9].push_back("*"+to_string(nent/sum_spdf[idx]));
+  for(unsigned int idx=0;idx<sum_mur.size();idx++)          var_val[10].push_back("*"+to_string(nent/sum_mur[idx]));
+  for(unsigned int idx=0;idx<sum_muf.size();idx++)          var_val[11].push_back("*"+to_string(nent/sum_muf[idx]));
+  for(unsigned int idx=0;idx<sum_murf.size();idx++)         var_val[12].push_back("*"+to_string(nent/sum_murf[idx]));
   //Calculate lepton weights
-  var_val[14].push_back("*"+to_string((nent-sum_wlep)/nent_zlep));
-  var_val[15].push_back("*"+to_string((nent-sum_fs_wlep)/nent_zlep));
+  var_val[13].push_back("*"+to_string((nent-sum_wlep)/nent_zlep));
+  var_val[14].push_back("*"+to_string((nent-sum_fs_wlep)/nent_zlep));
   for(unsigned int idx=0;idx<sum_slep.size();idx++)         
-    var_val[16].push_back("*"+to_string((nent-sum_slep[idx])/nent_zlep));
+    var_val[15].push_back("*"+to_string((nent-sum_slep[idx])/nent_zlep));
   for(unsigned int idx=0;idx<sum_fs_slep.size();idx++)      
-    var_val[17].push_back("*"+to_string((nent-sum_fs_slep[idx])/nent_zlep));
-  var_val[18].push_back("*"+to_string(w_lumi_corr));
+    var_val[16].push_back("*"+to_string((nent-sum_fs_slep[idx])/nent_zlep));
+  var_val[17].push_back("*"+to_string(w_lumi_corr));
 
 
   int totentries(0);

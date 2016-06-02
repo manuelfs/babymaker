629b9482e1cf4ab0634c792341bd33b552e20f44
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2016-06-01 17:02:25 -0700

    Updated script to send renormalization jobs

--- bmaker/genfiles/run/wrapper.sh
+++ bmaker/genfiles/run/wrapper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 
 DIRECTORY=`pwd`
-cd /net/cms2/cms2r0/babymaker/CMSSW_7_4_14/src/
+cd /net/cms2/cms2r0/babymaker/CMSSW_8_0_8/src/
 . /net/cms2/cms2r0/babymaker/cmsset_default.sh
 eval `scramv1 runtime -sh`
 cd $DIRECTORY;
 
 # Execute command
 $@
+

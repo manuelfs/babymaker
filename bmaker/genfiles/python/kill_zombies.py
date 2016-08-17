6988900976de1d41a71637b2dd5ddaa74f30094b
Adam Dishaw <aldishaw@gmail.com>
2016-08-17 12:44:58 -0700

    Add script to purge zombie ROOT files

---
+++ bmaker/genfiles/python/kill_zombies.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import argparse
+import os
+
+import ROOT
+
+def killZombies(in_dirs):
+    for d in in_dirs:
+        for root, dirs, files in os.walk(d):
+            for f in files:
+                path = os.path.join(root, f)
+                if os.path.splitext(f)[1] != ".root":
+                    continue
+                tfile = ROOT.TFile(path, "read")
+                kill = tfile.IsZombie() or not tfile.IsOpen()
+                tfile.Close()
+                if kill:
+                    os.remove(path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Recursively removes zombie ROOT files from given directories.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("in_dirs", nargs="*", default=["."],
+                        help="List of directories from which to purge zombie files.")
+    args = parser.parse_args()
+
+    killZombies(args.in_dirs)

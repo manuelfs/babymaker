c35188d09ccbcc1315f97493fbc6db9bba526b8e
adorsett20 <adorsett20@gmail.com>
2016-08-24 16:54:27 -0700

    cross_section higgs branching fraction fix

--- bmaker/genfiles/src/cross_sections.cc
+++ bmaker/genfiles/src/cross_sections.cc
@@ -176,42 +176,42 @@ namespace xsec{
   }
 
   void higgsinoCrossSection(int hig_mass, float &xsec, float &xsec_unc) {
-        if(hig_mass == 127) { xsec =  7602.2/1000; xsec_unc = 0.0393921; return;}
-        else if(hig_mass == 150) { xsec = 3832.31/1000; xsec_unc = 0.0413612; return;}
-        else if(hig_mass == 175) { xsec = 2267.94/1000; xsec_unc = 0.044299;  return;}
-        else if(hig_mass == 200) { xsec = 1335.62/1000; xsec_unc = 0.0474362; return;}
-        else if(hig_mass == 225) { xsec = 860.597/1000; xsec_unc = 0.0504217; return;}
-        else if(hig_mass == 250) { xsec = 577.314/1000; xsec_unc = 0.0532731; return;}
-        else if(hig_mass == 275) { xsec = 400.107/1000; xsec_unc = 0.0560232; return;}
-        else if(hig_mass == 300) { xsec = 284.855/1000; xsec_unc = 0.0586867; return;}
-        else if(hig_mass == 325) { xsec =  207.36/1000; xsec_unc = 0.0613554; return;}
-        else if(hig_mass == 350) { xsec = 153.841/1000; xsec_unc = 0.0640598; return;}
-        else if(hig_mass == 375) { xsec = 116.006/1000; xsec_unc = 0.066892;  return;}
-        else if(hig_mass == 400) { xsec = 88.7325/1000; xsec_unc = 0.0697517; return;}
-        else if(hig_mass == 425) { xsec = 68.6963/1000; xsec_unc = 0.0723531; return;}
-        else if(hig_mass == 450) { xsec = 53.7702/1000; xsec_unc = 0.0748325; return;}
-        else if(hig_mass == 475) { xsec = 42.4699/1000; xsec_unc = 0.0775146; return;}
-        else if(hig_mass == 500) { xsec = 33.8387/1000; xsec_unc = 0.0802572; return;}
-        else if(hig_mass == 525) { xsec = 27.1867/1000; xsec_unc = 0.0825803; return;}
-        else if(hig_mass == 550) { xsec = 21.9868/1000; xsec_unc = 0.0849278; return;}
-        else if(hig_mass == 575) { xsec = 17.9062/1000; xsec_unc = 0.087561;  return;}
-        else if(hig_mass == 600) { xsec = 14.6677/1000; xsec_unc = 0.0900693; return;}
-        else if(hig_mass == 625) { xsec =  12.062/1000; xsec_unc = 0.091959;  return;}
-        else if(hig_mass == 650) { xsec = 9.96406/1000; xsec_unc = 0.094065;  return;}
-        else if(hig_mass == 675) { xsec = 8.28246/1000; xsec_unc = 0.0957436; return;}
-        else if(hig_mass == 700) { xsec = 6.89981/1000; xsec_unc = 0.0982894; return;}
-        else if(hig_mass == 725) { xsec = 5.78355/1000; xsec_unc = 0.0999915; return;}
-        else if(hig_mass == 750) { xsec =  4.8731/1000; xsec_unc = 0.101211;  return;}
-        else if(hig_mass == 775) { xsec = 4.09781/1000; xsec_unc = 0.104646;  return;}
-        else if(hig_mass == 800) { xsec = 3.46143/1000; xsec_unc = 0.107618;  return;}
-        else if(hig_mass == 825) { xsec =  2.9337/1000; xsec_unc = 0.108353;  return;}
-        else if(hig_mass == 850) { xsec =  2.4923/1000; xsec_unc = 0.110016;  return;}
-        else if(hig_mass == 875) { xsec = 2.13679/1000; xsec_unc = 0.112636;  return;}
-        else if(hig_mass == 900) { xsec = 1.80616/1000; xsec_unc = 0.1134;    return;}
-        else if(hig_mass == 925) { xsec = 1.55453/1000; xsec_unc = 0.116949;  return;}
-        else if(hig_mass == 950) { xsec = 1.32692/1000; xsec_unc = 0.117027;  return;}
-        else if(hig_mass == 975) { xsec = 1.12975/1000; xsec_unc = 0.121244;  return;}
-        else if(hig_mass ==1000) { xsec = 0.968853/1000; xsec_unc = 0.126209; return;}
+        if(hig_mass == 127) { xsec = .5824*.5824*7602.2/1000; xsec_unc = 0.0393921; return;}
+        else if(hig_mass == 150) { xsec = .5824*.5824*3832.31/1000; xsec_unc = 0.0413612; return;}
+        else if(hig_mass == 175) { xsec = .5824*.5824*2267.94/1000; xsec_unc = 0.044299;  return;}
+        else if(hig_mass == 200) { xsec = .5824*.5824*1335.62/1000; xsec_unc = 0.0474362; return;}
+        else if(hig_mass == 225) { xsec = .5824*.5824*860.597/1000; xsec_unc = 0.0504217; return;}
+        else if(hig_mass == 250) { xsec = .5824*.5824*577.314/1000; xsec_unc = 0.0532731; return;}
+        else if(hig_mass == 275) { xsec = .5824*.5824*400.107/1000; xsec_unc = 0.0560232; return;}
+        else if(hig_mass == 300) { xsec = .5824*.5824*284.855/1000; xsec_unc = 0.0586867; return;}
+        else if(hig_mass == 325) { xsec = .5824*.5824* 207.36/1000; xsec_unc = 0.0613554; return;}
+        else if(hig_mass == 350) { xsec = .5824*.5824*153.841/1000; xsec_unc = 0.0640598; return;}
+        else if(hig_mass == 375) { xsec = .5824*.5824*116.006/1000; xsec_unc = 0.066892;  return;}
+        else if(hig_mass == 400) { xsec = .5824*.5824*88.7325/1000; xsec_unc = 0.0697517; return;}
+        else if(hig_mass == 425) { xsec = .5824*.5824*68.6963/1000; xsec_unc = 0.0723531; return;}
+        else if(hig_mass == 450) { xsec = .5824*.5824*53.7702/1000; xsec_unc = 0.0748325; return;}
+        else if(hig_mass == 475) { xsec = .5824*.5824*42.4699/1000; xsec_unc = 0.0775146; return;}
+        else if(hig_mass == 500) { xsec = .5824*.5824*33.8387/1000; xsec_unc = 0.0802572; return;}
+        else if(hig_mass == 525) { xsec = .5824*.5824*27.1867/1000; xsec_unc = 0.0825803; return;}
+        else if(hig_mass == 550) { xsec = .5824*.5824*21.9868/1000; xsec_unc = 0.0849278; return;}
+        else if(hig_mass == 575) { xsec = .5824*.5824*17.9062/1000; xsec_unc = 0.087561;  return;}
+        else if(hig_mass == 600) { xsec = .5824*.5824*14.6677/1000; xsec_unc = 0.0900693; return;}
+        else if(hig_mass == 625) { xsec = .5824*.5824* 12.062/1000; xsec_unc = 0.091959;  return;}
+        else if(hig_mass == 650) { xsec = .5824*.5824*9.96406/1000; xsec_unc = 0.094065;  return;}
+        else if(hig_mass == 675) { xsec = .5824*.5824*8.28246/1000; xsec_unc = 0.0957436; return;}
+        else if(hig_mass == 700) { xsec = .5824*.5824*6.89981/1000; xsec_unc = 0.0982894; return;}
+        else if(hig_mass == 725) { xsec = .5824*.5824*5.78355/1000; xsec_unc = 0.0999915; return;}
+        else if(hig_mass == 750) { xsec = .5824*.5824* 4.8731/1000; xsec_unc = 0.101211;  return;}
+        else if(hig_mass == 775) { xsec = .5824*.5824*4.09781/1000; xsec_unc = 0.104646;  return;}
+        else if(hig_mass == 800) { xsec = .5824*.5824*3.46143/1000; xsec_unc = 0.107618;  return;}
+        else if(hig_mass == 825) { xsec = .5824*.5824* 2.9337/1000; xsec_unc = 0.108353;  return;}
+        else if(hig_mass == 850) { xsec = .5824*.5824* 2.4923/1000; xsec_unc = 0.110016;  return;}
+        else if(hig_mass == 875) { xsec = .5824*.5824*2.13679/1000; xsec_unc = 0.112636;  return;}
+        else if(hig_mass == 900) { xsec = .5824*.5824*1.80616/1000; xsec_unc = 0.1134;    return;}
+        else if(hig_mass == 925) { xsec = .5824*.5824*1.55453/1000; xsec_unc = 0.116949;  return;}
+        else if(hig_mass == 950) { xsec = .5824*.5824*1.32692/1000; xsec_unc = 0.117027;  return;}
+        else if(hig_mass == 975) { xsec = .5824*.5824*1.12975/1000; xsec_unc = 0.121244;  return;}
+        else if(hig_mass ==1000) { xsec = .5824*.5824*0.968853/1000; xsec_unc = 0.126209; return;}
         else{ xsec = 0; xsec_unc = 0;}
   }
 

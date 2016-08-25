e32ea5eb362d482375f1c893e1d8cc0730dc8e08
adorsett20 <adorsett20@gmail.com>
2016-08-25 12:15:08 -0700

    Added higloose skim to skim_ntuples.cxx

--- bmaker/genfiles/src/skim_ntuples.cxx
+++ bmaker/genfiles/src/skim_ntuples.cxx
@@ -69,8 +69,12 @@ int main(int argc, char *argv[]){
     cuts = "ht>1000&&met<50&&(nvmus+nvels)==0";
   if(cuts=="qcd_njet10")
      cuts = "ht>1000&&met<50&&(nvmus+nvels)==0&&njets>=10";
-  if(cuts=="mm_std") cuts="Sum$(mm_nleps>=1&&mm_ht>500.&&mm_met>200.)>0";	     
-  if(cuts=="mm_std_nj5mj250") cuts="Sum$(mm_nleps>=1&&mm_ht>500&&mm_met>200&&mm_njets>=5&&mm_mj14_lep>250)>0||Sum$(mm_nleps>=1&&mm_ht>500&&mm_met>200&&mm_njets>=5&&mm_mj14_nolep>250)>0";
+  if(cuts=="mm_std")
+	 cuts="Sum$(mm_nleps>=1&&mm_ht>500.&&mm_met>200.)>0";	     
+  if(cuts=="mm_std_nj5mj250")
+	 cuts="Sum$(mm_nleps>=1&&mm_ht>500&&mm_met>200&&mm_njets>=5&&mm_mj14_lep>250)>0||Sum$(mm_nleps>=1&&mm_ht>500&&mm_met>200&&mm_njets>=5&&mm_mj14_nolep>250)>0";
+  if(cuts=="higloose")
+	cuts="met>100&&nvleps==0&&(njets==4||njets==5)&&nbm>=2";
 
   //// RA2/b skims
   TString pass="globalTightHalo2016Filter==1&&HBHENoiseFilter==1&&HBHEIsoNoiseFilter==1&&eeBadScFilter==1";
